02b82f741bb0cca4888628cca8c603037904150c
Chris Olstrom <chris@olstrom.com>
2016-11-20 19:39:50 -0800

    Add function to evaluate ruby code

--- src/mruby-scripting.c
+++ src/mruby-scripting.c
@@ -1,6 +1,7 @@
 #include "server.h"
 #include <mruby.h>
 #include <mruby/string.h>
+#include <mruby/proc.h>
 
 void mrbScriptingInit(int setup) {
   mrb_state *mrb = mrb_open();
@@ -33,4 +34,20 @@ void mrbAddReply(client *client, mrb_state *mrb, mrb_value value) {
   int   length = RSTRING_LEN(value);
 
   addReplyBulkCBuffer(client, string, length);
+}
+
+void mrbEvalCommand(client *client) {
+  mrbScriptingReset();
+
+         mrb_state        *mrb     = server.mrb;
+         mrbc_context     *context = mrbc_context_new(mrb);
+  struct mrb_parser_state *parser  = mrb_parse_string(mrb, client->argv[1]->ptr, context);
+  struct RProc            *proc    = mrb_generate_code(mrb, parser); /* TODO: Should be able to cache this... */
+         mrb_value         value   = mrb_run(mrb, proc, mrb_top_self(mrb));
+
+  mrbAddReply(client, mrb, value);
+
+  /* Cleanup */
+  mrb_pool_close(parser->pool);
+  mrbc_context_free(mrb, context);
 }
